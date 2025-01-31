76e4b4839e852b5009088172cdd078fef7759f4a
Leon C <leon.cl2025@gmail.com>
2025-01-31 16:52:17 +0800

    binary tree

--- 0124.ts
+++ 0124.ts
@@ -1,13 +1,5 @@
-class TreeNode {
-     val: number
-     left: TreeNode | null
-     right: TreeNode | null
-     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-         this.val = (val===undefined ? 0 : val)
-         this.left = (left===undefined ? null : left)
-         this.right = (right===undefined ? null : right)
-     }
- }
+import { TreeNode } from "./tree/treeNode";
+import { buildTree } from "./tree/treeNode.utils";
 
 const MIN_VALUE = -Infinity;
 function maxPathSum(root: TreeNode | null): number {
@@ -97,29 +89,6 @@ function cloneTree(root: TreeNode | null): TreeNodeEnhanced | null {
     node.left = leftClone;
     node.right = rightClone;
     return node;
-}
-
-function buildTree(arr: number[]): TreeNode | null {
-    if (arr.length === 0) {
-        return null;
-    }
-    const root = new TreeNode(arr[0]);
-    const queue = [root];
-    let i = 1;
-    while (i < arr.length) {
-        const node = queue.shift();
-        if (i < arr.length && arr[i] !== null) {
-            node.left = new TreeNode(arr[i]);
-            queue.push(node.left);
-        }
-        i++;
-        if (i < arr.length && arr[i] !== null) {
-            node.right = new TreeNode(arr[i]);
-            queue.push(node.right);
-        }
-        i++;
-    }
-    return root;
 }
 
 function printNode(node: TreeNodeEnhanced) {
